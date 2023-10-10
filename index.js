e1bf1129ee461bbfb1e48653918208c1891e4e6d
Partha <parthasdeyofficial@gmail.com>
2023-10-10 21:06:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
+app.get('/', (req, res) => {
+    console.log("Homepage has been loaded");
+    res.render('index.ejs')
+});
+
+app.listen(port, () => {
+    console.log("[INFO] Express app is running on http://localhost:8080");
+});
